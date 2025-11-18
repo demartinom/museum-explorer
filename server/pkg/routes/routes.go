a06c7201b1857ea12a27ecb1d685c6f47d10dfb2
Matt DeMartino <matt.s.demartino@gmail.com>
2025-11-18 16:52:12 -0500

    Created met specific api route

--- server/pkg/routes/routes.go
+++ server/pkg/routes/routes.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"github.com/demartinom/museum-explorer/server/pkg/handlers"
+	metHandlers "github.com/demartinom/museum-explorer/server/pkg/handlers/met"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -10,7 +10,7 @@ func RegisterRoutes() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("api/met/departments", handlers.MetDepartments)
+	r.Route("/api/met", func(r chi.Router) { r.Get("api/met/departments", metHandlers.MetDepartments) })
 
 	return r
 }
