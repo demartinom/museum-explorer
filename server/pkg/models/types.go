adf35a4c11d3c6d5024963b30584a861fabc920a
Matt DeMartino <matt.s.demartino@gmail.com>
2025-11-18 15:20:46 -0500

    Types for met departments

---
+++ server/pkg/models/types.go
@@ -0,0 +1,10 @@
+package models
+
+type MetDepartments struct {
+	Departments []MetSingleDepartment `json:"departments"`
+}
+
+type MetSingleDepartment struct {
+	DepartmentID int    `json:"departmentId"`
+	DisplayName  string `json:"displayName"`
+}
