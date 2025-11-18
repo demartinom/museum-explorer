b7fb5a615c2b95b029d4a2ed1578a5a2692e92b6
Matt DeMartino <matt.s.demartino@gmail.com>
2025-11-18 14:57:18 -0500

    Implement chi to create server

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	// Creates server at port 3000
+	r := chi.NewRouter()
+	port := os.Getenv("PORT")
+
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Museum Explorer Backend"))
+	})
+
+	http.ListenAndServe(port, r)
 }
