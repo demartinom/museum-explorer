0c2b92a0d840beddcb03f55309c7a72c90005829
Matt DeMartino <matt.s.demartino@gmail.com>
2025-11-24 15:22:44 -0500

    Create functions for retrieving random highlight and one artwork based on id

--- server/pkg/services/metservices/metClient.go
+++ server/pkg/services/metservices/metClient.go
@@ -3,6 +3,7 @@ package metservices
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 )
 
@@ -28,5 +29,49 @@ func (c *MetClient) GetDepartments() (*DepartmentsResponse, error) {
 		return nil, err
 	}
 
+	return &result, nil
+}
+
+// Gets a list of Art IDs for the highlights of the Met and then returns a random artwork from the highlights
+func (c *MetClient) GetRandom() (*MetSingleArtwork, error) {
+	url := fmt.Sprintf("%s/search?q=isHightlight=true", c.BaseURL)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var randomResult GetHighlightIDs
+	if err := json.NewDecoder(resp.Body).Decode(&randomResult); err != nil {
+		return nil, err
+	}
+
+	// TODO: Ensure it doesn't return something blank or with no picture
+	// Selects a random ID
+	randArtworkID := rand.Intn(len(randomResult.ObjectIds))
+
+	// Gets information for randomly selected artwork
+	result, err := c.GetSpecific(randomResult.ObjectIds[randArtworkID])
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// Searches for specific work in collection using it's unique object ID
+func (c *MetClient) GetSpecific(objectID int) (*MetSingleArtwork, error) {
+	url := fmt.Sprintf("%s/objects/%d", c.BaseURL, objectID)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var result MetSingleArtwork
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
 	return &result, nil
 }
