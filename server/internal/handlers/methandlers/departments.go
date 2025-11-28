d4cebf102606f8df21ac754568c2ce472f18b5f9
Matt DeMartino <matt.s.demartino@gmail.com>
2025-11-28 13:56:01 -0500

    Use handler func so able to use metclient from main.go

--- server/internal/handlers/methandlers/departments.go
+++ server/internal/handlers/methandlers/departments.go
@@ -4,24 +4,24 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/demartinom/museum-explorer/server/internal/museums/met"
+	"github.com/demartinom/museum-explorer/server/internal/museums/metmuseum"
 )
 
-// Client to be used by all Met handlers
-var client = met.NewClient()
-
 // Handler that makes API call to department endpoint on Met API
-func DepartmentsHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := client.GetDepartments()
-	if err != nil {
-		http.Error(w, "Failed to fetch Met API", http.StatusBadGateway)
-		return
-	}
+// Uses handlerfunc to pass in met client from main.go
+func DepartmentsHandler(client *metmuseum.MetClient) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := client.GetDepartments()
+		if err != nil {
+			http.Error(w, "Failed to fetch Met API", http.StatusBadGateway)
+			return
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK) // optional, default is 200
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK) // optional, default is 200
 
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		}
 	}
 }
--- server/internal/handlers/methandlers/random.go
+++ server/internal/handlers/methandlers/random.go
@@ -3,20 +3,25 @@ package methandlers
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/demartinom/museum-explorer/server/internal/museums/metmuseum"
 )
 
 // Handler that makes API call return random artwork from the Met's highlights for the Artwork of the day
-func RandomHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := MetClient.GetRandomArtwork()
-	if err != nil {
-		http.Error(w, "Failed to fetch Met API", http.StatusBadGateway)
-		return
-	}
+// Uses handlerfunc to pass in met client from main.go
+func RandomHandler(client *metmuseum.MetClient) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := client.FetchRandom()
+		if err != nil {
+			http.Error(w, "Failed to fetch Met API", http.StatusBadGateway)
+			return
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK) // optional, default is 200
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK) // optional, default is 200
 
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		}
 	}
 }
