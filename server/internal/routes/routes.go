7d0d9657b8fc9396b179ef55e1a930e059eb32ce
Matt DeMartino <matt.s.demartino@gmail.com>
2025-11-28 13:56:15 -0500

    Take in metclient and pass to handlers

--- server/internal/routes/routes.go
+++ server/internal/routes/routes.go
@@ -2,20 +2,22 @@ package routes
 
 import (
 	"github.com/demartinom/museum-explorer/server/internal/handlers/methandlers"
+	"github.com/demartinom/museum-explorer/server/internal/museums/metmuseum"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 // Creates routes for different API calls on server startup
-func RegisterRoutes() *chi.Mux {
+// Takes in metClient from main.go and passes it into handlers
+func RegisterRoutes(metClient *metmuseum.MetClient) *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	// Registers routes that belong to the met
 	// They will all fall under /api/met
 	r.Route("/api/met", func(r chi.Router) {
-		r.Get("/departments", methandlers.DepartmentsHandler)
-		r.Get("/random", methandlers.RandomHandler)
+		r.Get("/departments", methandlers.DepartmentsHandler(metClient))
+		r.Get("/random", methandlers.RandomHandler(metClient))
 	})
 
 	return r
