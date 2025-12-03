204bcfeaa743c62f9ce5aff947a5a43d8a3287e7
Matt DeMartino <matt.s.demartino@gmail.com>
2025-12-03 16:07:11 -0500

    Create page with list of museums who's APIs are on website

--- client/app/museums/page.tsx
+++ client/app/museums/page.tsx
@@ -1,7 +1,19 @@
-export default function Museums() {
+import { apiGet } from "@/lib/apiGet";
+import { Museumlist } from "@/types/museum-list";
+
+export default async function Museums() {
+  const museumArray = await apiGet<Museumlist>("/museums");
+  const museumlist = museumArray.map((museum) => {
+    return (
+      <a href={`/museums/${museum.slug}`} key={museum.slug}>
+        <p>{museum.name}</p>
+      </a>
+    );
+  });
   return (
     <div>
       <h1>Explore Museum Collections</h1>
+      {museumlist}
     </div>
   );
 }
--- client/types/museum-list.ts
+++ client/types/museum-list.ts
@@ -1,3 +1,4 @@
+// Interface for taking array of museums from backend and putting them in objects
 interface Museum {
   name: string;
   slug: string;
